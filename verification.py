25bec57720c252a310a25f3910f0ed69fc116564
inion13 <inion13@gmail.com>
2023-11-01 00:45:02 +0800

    update verification

--- verification.py
+++ verification.py
@@ -15,14 +15,14 @@ def get_weather(latitude, longitude):
     print(f'''
 Weather at the desired coordinates ({r['daily']['time'][0]}):
     Temperature: {r['current']['temperature_2m']}{r['current_units']['temperature_2m']} (Apparent Temperature: \
-{r['current']['apparent_temperature']}{r['current_units']['apparent_temperature']})
+{r['current']['apparent_temperature']}{r['current_units']['apparent_temperature']}
     Precipitation: {r['current']['precipitation']} {r['current_units']['precipitation']}
         Rain: {r['current']['rain']} {r['current_units']['rain']}
         Showers: {r['current']['showers']} {r['current_units']['showers']}
-        Snowfall: {r['current']['snowfall']} {r['current_units']['snowfall']}')
-    Cloudcover Total: {r['current']['cloudcover']} {r['current_units']['cloudcover']}')
-    Surface Pressure: {int(r['current']['surface_pressure']) / 1.333:.1f} mmHg')
-    Wind Speed: {r['current']['windspeed_10m']} {r['current_units']['windspeed_10m']}')
+        Snowfall: {r['current']['snowfall']} {r['current_units']['snowfall']}
+    Cloudcover Total: {r['current']['cloudcover']} {r['current_units']['cloudcover']}
+    Surface Pressure: {int(r['current']['surface_pressure']) / 1.333:.1f} mmHg
+    Wind Speed: {r['current']['windspeed_10m']} {r['current_units']['windspeed_10m']}
 Sunrise at {r['daily']['sunrise'][0][-5:]}, sunset at {r['daily']['sunset'][0][-5:]} by Moscow time
 ''')
 
